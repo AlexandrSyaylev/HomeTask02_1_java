cca2f60972e98e108b84f09d5267e13997fe6357
Alexander Syayalev <alexxs@inbox.ru>
2022-01-08 00:33:51 +0300

    -A

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//Lection 2 Hometask 2.1 about Bonus Miles
+public class Main {
+    public static void main(String[] args) {
+        int ticketCoast =2011;
+        int BonusMiles = ticketCoast/20;
+
+        System.out.println("За Вашу покупку на " + ticketCoast  +" рублей Вам будет начисленно " + BonusMiles +" Бонусных Миль");
+    }
+}
